4bcf5d0281cb8d12575078958dc86f89d74ae962
Muhammad Ahmed Villa Khan <m.ahmedk287@gmail.com>
2023-04-09 16:18:10 +0500

    refactor: helper class is refactored ♻️

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# node-express-helper
+# Node.js Helper Functions
+This project contains a collection of helper functions that can be used in other Node.js projects. The project has been designed to be modular, meaning that each function can be easily used on its own without requiring any additional code.
+
+## Features
+- User CRUD operations (create, read, update, delete)
+- Mongoose ORM for database integration
+- Swagger UI for API documentation
+- Third-party middlewares (helmet, xss-clean, cors, and express rate limit) for security and performance improvements
+- A generic response module for all CRUD operations
+- Log all errors using Winston logger
+- Store logs in a file and automatically delete after 3 days
+
+## Getting Started
+To get started with this project, follow these steps:
+
+1. Clone the repository: git clone https://github.com/m-ahmedk/node-mongo-api.git
+2. Install dependencies: npm install
+3. Start the server: npm start
+4. Access the Swagger UI documentation in your web browser at http://localhost:3002
+
+## Prerequisites
+To run this project, you need:
+
+- Node.js
+- MongoDB
+- npm
+
+## Configuration
+This project uses environment variables for configuration. The following variables are required:
+
+- MONGO_URI: the URI of the MongoDB database to use
+- PORT: default port to run the local host
+
+You can set these variables in a .env file in the root directory of the project.
+
+## Built With
+Node.js
+Express
+Mongoose
+Winston
+Swagger
+
+## Contributing
+If you would like to contribute to the project, please reach out to the repository owner.
+
+## License
+This project is licensed under the MIT License - see the [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://github.com/m-ahmedk/node-mongo-api/blob/main/LICENSE) file for details.
--- utils/helper.js
+++ utils/helper.js
@@ -1,10 +1,23 @@
 const helpers = {};
 
+/**
+ * Returns true if the text starts with "http", indicating it is a valid URL
+ *
+ * @param {string} text - The text to check
+ * @returns {boolean} - True if the text starts with "http", false otherwise
+ */
 helpers.isValidURL = function (text) {
     return text.startsWith("http");
 };
 
-helpers.generateRandomString = function () {
+
+/**
+ * Generates a random string of alphanumeric characters of length n
+ *
+ * @param {number} n - The length of the random string to generate
+ * @returns {string} - The random string
+ */
+helpers.generateRandomString = function (n) {
     var result = '';
     var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     var charactersLength = characters.length;
@@ -15,14 +28,36 @@ helpers.generateRandomString = function () {
     return result;
 };
 
+
+/**
+ * Returns true if the provided ID is a valid MongoDB ObjectID, false otherwise
+ *
+ * @param {string} id - The ID to check
+ * @returns {boolean} - True if the ID is valid, false otherwise
+ */
 helpers.isValidObjectId = function (id) {
     return mongoose.Types.ObjectId.isValid(id);
 };
 
+
+/**
+ * Returns true if the value is null, undefined, or an empty string, false otherwise
+ *
+ * @param {*} value - The value to check
+ * @returns {boolean} - True if the value is null, undefined, or an empty string, false otherwise
+ */
 helpers.isNullOrUndefined = function (value) {
     if (value === undefined || value === null || value === "") return true;
+    else return false;
 };
 
+
+/**
+ * Trims all string values in an object
+ *
+ * @param {object} obj - The object whose string values to trim
+ * @returns {Promise<array>} - A promise that returns an array of [key, value] pairs where the value is trimmed
+ */
 helpers.trim_fields = async function (obj) {
     var list = Object.entries(obj);
     let array = new Array();
@@ -35,6 +70,13 @@ helpers.trim_fields = async function (obj) {
     return array;
 }
 
+
+/**
+ * Converts a string to a slug format, replacing non-alphanumeric characters with hyphens
+ *
+ * @param {string} text - The text to convert
+ * @returns {Promise<string>} - A promise that returns a converted slug text
+ */
 helpers.convertToSlug = async function (text) {
     return text
         .toLowerCase()
@@ -42,6 +84,13 @@ helpers.convertToSlug = async function (text) {
         .replace(/ +/g, "-");
 }
 
+
+/**
+ * Calculates the age based on the provided date of birth
+ *
+ * @param {string} dateOfBirth - The date of birth in string format (e.g. "2000-01-01")
+ * @returns {Promise<number>} - A promise that returns the calculated age
+ */
 helpers.calculateAge = async function (dateOfBirth) {
     const dob = new Date(dateOfBirth);
     const ageDiffMs = Date.now() - dob.getTime();
@@ -49,11 +98,23 @@ helpers.calculateAge = async function (dateOfBirth) {
     return Math.abs(ageDate.getUTCFullYear() - 1970);
 }
 
+
+/**
+ * Validates an email address.
+ * @param {string} email - The email address to validate.
+ * @returns {Promise<boolean>} - A promise that resolves to a boolean indicating whether the email address is valid or not.
+ */
 helpers.validateEmail = async function (email) {
     const re = /\S+@\S+\.\S+/;
     return re.test(email);
 }
 
+
+/**
+ * Formats a date in the format "YYYY-MM-DD".
+ * @param {string|Date} date - The date to format.
+ * @returns {Promise<string>} - A promise that resolves to a formatted date string.
+ */
 helpers.formatDate = async function (date) {
     const d = new Date(date);
     const year = d.getFullYear();
@@ -62,6 +123,14 @@ helpers.formatDate = async function (date) {
     return `${year}-${month}-${day}`;
 }
 
+
+/**
+ * Paginates an array of results.
+ * @param {number} page - The page number to retrieve.
+ * @param {number} pageSize - The number of items to include per page.
+ * @param {Array} results - The array of results to paginate.
+ * @returns {Promise<Array>} - A promise that resolves to an array of paginated results.
+ */
 helpers.paginateResults = async (page, pageSize, results) => {
     const startIndex = (page - 1) * pageSize;
     const endIndex = startIndex + pageSize;
@@ -69,7 +138,6 @@ helpers.paginateResults = async (page, pageSize, results) => {
     const paginatedResults = results.slice(startIndex, endIndex);
     return paginatedResults;
 }
-
 
 
 module.exports = helpers;
